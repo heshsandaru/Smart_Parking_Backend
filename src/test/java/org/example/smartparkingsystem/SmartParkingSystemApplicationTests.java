7a77b0aa0758828c0e4f42d0736efdcb68b69a0b
Heshan <heshansandaruwan999@gmail.com>
2026-01-22 13:10:03 +0530

    first commit

---
+++ src/test/java/org/example/smartparkingsystem/SmartParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartparkingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
